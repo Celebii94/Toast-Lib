5f7ac5a0240598dcbc2ccb21a082177812bc48fe
Celebii94 <112369018+Celebii94@users.noreply.github.com>
2022-08-29 15:44:10 +0700

    Update ToastMessage.java

--- mylibrary/src/main/java/com/demo/mylibrary/ToastMessage.java
+++ mylibrary/src/main/java/com/demo/mylibrary/ToastMessage.java
@@ -3,12 +3,9 @@ package com.demo.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-    public class ToastMessage {
-
-        public void s(Context c, String message){
-
-            Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
-
-        }
-
+public class ToastMessage {
+    public static void show(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
 }
+
