b6c43ecda4e34a94408534c9b707856f9315e15d
Celebii94 <112369018+Celebii94@users.noreply.github.com>
2022-08-29 15:37:57 +0700

    Update ToastMessage.java

--- mylibrary/src/main/java/com/demo/mylibrary/ToastMessage.java
+++ mylibrary/src/main/java/com/demo/mylibrary/ToastMessage.java
@@ -3,7 +3,7 @@ package com.demo.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-    public class ToasterMessage {
+    public class ToastMessage {
 
         public void s(Context c, String message){
 
