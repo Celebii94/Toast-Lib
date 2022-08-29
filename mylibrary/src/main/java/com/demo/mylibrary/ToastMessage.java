a90d726decd279af2d5c3f6033945010d8478d90
celebii94 <chicong161294@gmail.com>
2022-08-29 14:24:29 +0700

    first commit

---
+++ mylibrary/src/main/java/com/demo/mylibrary/ToastMessage.java
@@ -0,0 +1,15 @@
+package com.demo.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+    public class ToasterMessage {
+
+        public void s(Context c, String message){
+
+            Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
